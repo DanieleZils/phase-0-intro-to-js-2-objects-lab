5ed65879fe6abd9536457a9d7683e65dbc24479e
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2022-12-23 16:12:42 -0500

    commit tree 1cd8720b98cb898e474a63d7f3d24ac2bc39d0b3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name : "Hugh",
+    streetAddress : "Madison ave",
+  };
+  
+//   updateEmployeeWithKeyAndValue(employee, key, value)
+//       1) returns an employee with the original key value pairs and the new key value pair
+//       2) it does not modify the original employee, but rather returns a clone with the new data
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj };
+    newObj[key] = value;
+    return newObj;
+};
+
+// destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+// updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee:
+// ReferenceError: destructivelyUpdateEmployeeWithKeyAndValue is not defined
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    
+    return obj;
+};
+//  deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+//       2) does not modify the original employee (it is non-destructive)
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj };
+    delete newObj.name;
+    return newObj;
+};
+
+// destructivelyDeleteFromEmployeeByKey(employee, key)
+//       1) returns employee without the deleted key/value pair
+//       2) modifies the original employee
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj.name;
+    return obj
+}
